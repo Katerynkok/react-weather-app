11664d7fcd18db9ff4f041bc1d4347f9a0d25ba4
Kateynkok <dream.is.in.me@gmail.com>
2023-09-07 15:49:29 +0000

    added GitHub repository  link

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Kateryna Melanych and open-sourced on{" "}
+        <a
+          href="https://github.com/Katerynkok/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
