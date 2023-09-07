fdfaa3ba676d4fa56d5a5ca988c49c52f49f5574
Kateynkok <dream.is.in.me@gmail.com>
2023-09-07 16:36:33 +0000

    added boostrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Kateryna Melanych and open-sourced on{" "}
-        <a
-          href="https://github.com/Katerynkok/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Kateryna Melanych and open-sourced on{" "}
+          <a
+            href="https://github.com/Katerynkok/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
