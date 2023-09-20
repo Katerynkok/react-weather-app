ec11fc7ea38bf16d6d5763f78967f7cfeb29b264
Kateynkok <dream.is.in.me@gmail.com>
2023-09-20 21:35:25 +0000

    added API call for the forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "5t377bdod73f4cbfd4df58abbb0a0579";
+  let city = props.city;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
@@ -11,11 +21,9 @@ export default function WeatherForecast(props) {
           <div className="clearfix">
             <img
               src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-              alt="broken-clouds-day"
-              size={36}
+              alt="overcast clouds"
             ></img>
           </div>
-
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperaure-max">19°</span>
             <span className="WeatherForecast-temperaure-min">10°</span>
